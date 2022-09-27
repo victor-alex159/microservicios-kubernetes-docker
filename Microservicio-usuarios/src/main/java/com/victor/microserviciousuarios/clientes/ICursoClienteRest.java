e29f47fc203b0e083e48f1bd62b2f1fa6a54110d
Victor Alexander <alexbenavente322@gmail.com>
2022-09-27 11:59:19 -0500

    Se agrego Dockerfile

--- Microservicio-usuarios/src/main/java/com/victor/microserviciousuarios/clientes/ICursoClienteRest.java
+++ Microservicio-usuarios/src/main/java/com/victor/microserviciousuarios/clientes/ICursoClienteRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservicio-cursos", url = "localhost:8002")
+@FeignClient(name = "microservicio-cursos", url = "host.docker.internal:8002")
 public interface ICursoClienteRest {
 
     @DeleteMapping("/curso/eliminar-curso-usuario/{usuarioId}")
